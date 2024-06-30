6b7267dffa22e275c2ab08501774b232100e7ba1
M.N. <niz.moussatat@gmail.com>
2024-06-30 17:29:41 +0200

    Deployed 4b81e7b with MkDocs version: 1.6.0

--- xtra/javascripts/extra.js
+++ xtra/javascripts/extra.js
@@ -2,9 +2,15 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);} 
 
 $(".pdf").on('click', function() {   
-		gtag('event', 'file_download', {
+		var filepath = {{Click URL}}.split("/");
+		var filename = filepath.pop();
+		
+		gtag('event', 'file_download', { 
 			  'event_category' : 'send-to',
-			  'event_label' : 'downloads'
+			  'event_label' : 'downloads',
+			  'file_extension' : 'pdf' ,
+			  'file_name' : {{ filename }},
+			  ; 
 			});
 	});
 	
