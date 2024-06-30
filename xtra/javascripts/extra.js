ba5373173140577b24955d9df5c8d84df7fae413
M.N. <niz.moussatat@gmail.com>
2024-06-30 16:56:33 +0200

    Deployed addd9cd with MkDocs version: 1.6.0

--- xtra/javascripts/extra.js
+++ xtra/javascripts/extra.js
@@ -2,7 +2,6 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);} 
 
 $(".pdf").on('click', function() {   
-		gtag('config', {{ analytics.property }} );
 		gtag('event', 'file_download', {
 			  'event_category' : 'send-to',
 			  'event_label' : 'downloads'
