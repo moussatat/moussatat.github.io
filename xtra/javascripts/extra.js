644b4c3c1472c4f15d86fa7e06a4124cdae7d88d
M.N. <niz.moussatat@gmail.com>
2024-06-30 20:33:22 +0200

    Deployed 75e8c7e with MkDocs version: 1.6.0

--- xtra/javascripts/extra.js
+++ xtra/javascripts/extra.js
@@ -1,7 +1,7 @@
-window.dataLayer = window.dataLayer || []; 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-$(".pdf").on('click', function() {    
-		gtag('event', 'file_download'  ); 
-	});
-	
-
+ga('create', 'G-BYF4FEE2HD', 'auto');
+ga('send', 'file_download');
