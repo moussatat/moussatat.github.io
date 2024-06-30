d7c7f61fc4505549b8c23b5453322e0fa00c70de
M.N. <niz.moussatat@gmail.com>
2024-06-30 19:40:12 +0200

    Deployed 6d740a8 with MkDocs version: 1.6.0

--- xtra/javascripts/extra.js
+++ xtra/javascripts/extra.js
@@ -1,7 +1,6 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', { analytics.property })
+gtag('js', new Date()); 
 $(".pdf").on('click', function() {    
 		gtag('event', 'file_download'  ); 
 	});
