59d12c56b7612a087b325951759b57767806fc95
M.N. <niz.moussatat@gmail.com>
2024-06-30 15:38:07 +0200

    Deployed d07fff7 with MkDocs version: 1.6.0

--- xtra/javascripts/extra.js
+++ xtra/javascripts/extra.js
@@ -1,1 +1,9 @@
-
+	 
+<script id="__analytics"> 
+$(".ext-download-file").on('click', function() { 
+		gtag('event', 'file_download', {
+			  'event_category' : 'send-to',
+			  'event_label' : 'downloads'
+			});
+	});
+</script>
